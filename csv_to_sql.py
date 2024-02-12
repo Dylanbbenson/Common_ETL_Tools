f82bb5d6b0aaafbee7381f9600f26f11c59c7816
Dylan Benson <dylanbenson@tutanota.com>
2024-02-12 14:03:13 -0600

    Updated script to infer data types for columns

--- csv_to_sql.py
+++ csv_to_sql.py
@@ -2,29 +2,57 @@ import csv
 import sys
 import os
 
+def infer_data_types(file_path):
+    with open(file_path, 'r') as csv_file:
+        csv_sniffer = csv.Sniffer()
+        sample_data = csv_file.read(1024) 
+        dialect = csv_sniffer.sniff(sample_data)
+        csv_file.seek(0) 
 
+        csv_reader = csv.reader(csv_file, dialect)
+        header = next(csv_reader) 
+
+        # Infer column data types
+        type_mapping = {}
+        for col in header:
+            type_mapping[col] = 'VARCHAR(255)'  
+
+        for row in csv_reader:
+            for col, value in zip(header, row):
+                try:
+                    int(value)
+                    type_mapping[col] = 'INT'
+                except ValueError:
+                    try:
+                        float(value)
+                        type_mapping[col] = 'NUMERIC' 
+                    except ValueError:
+                        pass 
+
+    return type_mapping
 
 def csv_to_mysql(file_path):
     table_name = file_path.split('.')[0]
+    type_mapping = infer_data_types(file_path)
 
     with open(file_path, 'r') as csv_file:
         csv_reader = csv.reader(csv_file)
-        header = next(csv_reader)  # Get the header
+        header = next(csv_reader) 
 
-        create_table_statement = f"CREATE TABLE {table_name} (\n"
-        create_table_statement += ', '.join(f'{col} VARCHAR(255)' for col in header)
-        create_table_statement += '\n);'
+        create_table_statement = f"CREATE TABLE {table_name} ("
+        create_table_statement += ', '.join(f'{col} {type_mapping[col]}' for col in header)
+        create_table_statement += ');\n'
 
-        sql_file = "./data/"+table_name+".sql"
-
-        with open(sql_file, 'w') as file:
+        with open("./data/"+table_name+".sql", 'w') as file:
             file.write(create_table_statement)
 
             for row in csv_reader:
                 insert_statement = f"INSERT INTO {table_name} ({', '.join(header)}) VALUES ("
-                insert_statement += ', '.join(f"'{value}'" for value in row)
-                insert_statement += ');\n'  # Add a newline character after each insert statement
+                insert_statement += ', '.join(value if type_mapping[col] in ['INT', 'NUMERIC'] else f"'{value}'" for col, value in zip(header, row))
+                insert_statement += ');\n'
                 file.write(insert_statement)
+
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
@@ -33,6 +61,6 @@ if __name__ == "__main__":
 
     directory_path = './data/'
     os.makedirs(directory_path, exist_ok=True)
-    
+
     csv_file_path = sys.argv[1]
     csv_to_mysql(csv_file_path)
